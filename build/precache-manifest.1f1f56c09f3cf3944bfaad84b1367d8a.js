ad36b9b2499ae5650919e8278acf901b6207e13c
JJGlamour <88106827@qq.com>
2020-06-22 11:57:35 +0800

    build

---
+++ build/precache-manifest.1f1f56c09f3cf3944bfaad84b1367d8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddaf794dcfa3f1179ef653798d4bbf12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f05313eee18e553a32",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "b00227ae7725584c15ec",
+    "url": "/static/css/main.a855a380.chunk.css"
+  },
+  {
+    "revision": "e0f05313eee18e553a32",
+    "url": "/static/js/2.cd65b957.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cd65b957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00227ae7725584c15ec",
+    "url": "/static/js/main.1a15821c.chunk.js"
+  },
+  {
+    "revision": "c671c226882c8f8407bb",
+    "url": "/static/js/runtime-main.9094e1e7.js"
+  }
+]);
